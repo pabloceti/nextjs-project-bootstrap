f4ce1d5442dfa418cf0860817200b47d4e27a0b7
pabloceti <94643666+pabloceti@users.noreply.github.com>
2025-08-14 13:34:14 +0200

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1755171237373

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_ca4e0904._.js",
+  "static/chunks/node_modules_189af304._.js"
+],
+    source: "dynamic"
+});
